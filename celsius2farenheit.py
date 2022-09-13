2ff13748242a3764f99a6450dedf09fc21594b50
Luis Fernando Alvarez Alvarez (24284) <luis.alvarez@est.zamorano.edu>
2022-09-12 21:00:26 -0600

    Commit versión 1

---
+++ celsius2farenheit.py
@@ -0,0 +1,5 @@
+# Programa que traslada el valor de Celsius a Farenheit
+
+tempCelsius = input (" Temperatura en Celsius ")
+valorFarenheit = 9/5 * float (tempCelsius) + 32
+print(valorFarenheit)   
---
+++ pagoporhora.py
@@ -0,0 +1,6 @@
+# Programa que calcula el valor de pago por hora
+
+horasdetrabajo = input (" Horas trabajadas ")
+valorporhora = input (" valor por hora ")
+pago = float (horasdetrabajo) * int (valorporhora)
+print(pago) 
