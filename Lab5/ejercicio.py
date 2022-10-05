8dd7c86702bfc372afd4483159ccc16d859f60ea
Luis Alvarez <112092550+L-Alvarez08@users.noreply.github.com>
2022-10-05 08:36:32 -0600

    Add files via upload

---
+++ Lab5/ejercicio.py
@@ -0,0 +1,10 @@
+from statistics import linear_regression
+
+
+try:
+  entrada = input("Ingrese el nombre del archivo: ")
+  archivo = open(entrada, "r", encoding= "UTF-8")
+  for línea in archivo:
+    print(línea.upper())
+except:
+  print("Error, archivo no existe")
